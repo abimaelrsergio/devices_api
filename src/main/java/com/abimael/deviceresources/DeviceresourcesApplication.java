9a5c71593252d9872aff219703efab2e2589cc0f
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-04-28 11:36:46 -0300

    Dependencies added

---
+++ src/main/java/com/abimael/deviceresources/DeviceresourcesApplication.java
@@ -0,0 +1,13 @@
+package com.abimael.deviceresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceresourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceresourcesApplication.class, args);
+	}
+
+}
